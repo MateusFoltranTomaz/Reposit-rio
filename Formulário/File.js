4d6479e23618c41163fe47789cfe3b9dbae85ed5
MateusFoltran <142942489+MateusFoltranTomaz@users.noreply.github.com>
2024-07-10 16:43:24 -0300

    Add files via upload

--- Formulário/File.js
+++ Formulário/File.js
@@ -3,6 +3,7 @@ function Cadastrar(){
     let nomeUsuario = document.getElementById('Nome').value;
     let emailUsuario = document.getElementById('Email').value;
     let senhaUsuario = document.getElementById('Senha').value;
+    
     let confereDados = document.querySelector("input[name='Dados']:checked").value;
 
     alert('Seu nome é: ' + nomeUsuario + '\n' 
@@ -10,6 +11,12 @@ function Cadastrar(){
     + 'Sua senha é: ' + senhaUsuario + '\n'
     + 'Você confirmou os dados: ' + confereDados + '\n');
 
-    
+    window.location.href='Pagina.html'
+
+}
+
+function TelaCadastro(){
+
+    window.location.href='Index.html'
 
 }
